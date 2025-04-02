1e93db19d4e6132824a3dfd83dd33c1981fe4389
IamSaransh <saranshvashistha@gmail.com>
2025-04-03 01:55:37 +0530

    added loop for plantvillage

--- examples/dreambooth/train_dreambooth_plants.py
+++ examples/dreambooth/train_dreambooth_plants.py
@@ -1472,7 +1472,6 @@ if __name__ == "__main__":
     for cls in config["classes"]:
         class_params = cls["params_indv"]
         print(f"class = {cls}")
-        print(config)
         
         # Create new args dictionary
         new_args = argparse.Namespace(**common_params, **class_params)
